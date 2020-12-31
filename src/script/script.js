6273dc63ef4dc4db6529def4bcf388a61a8800fc
Sujauddin <imsuem@tutanota.com>
2020-12-31 11:07:55 +0530

    fix

--- src/script/script.js
+++ src/script/script.js
@@ -2,7 +2,7 @@ let hiddenLink = document.querySelectorAll('.hida')
 let downArrowDiv = document.querySelector('.aside__navarrow')
 let settingsDiv = document.querySelector('.header__stdiv')
 let settingSvg = document.querySelector('.header__settings')
-
+let themeChanger  = document.querySelector('#themechanger');
 
 downArrowDiv.addEventListener('click', function(){
     for(let i = 0; i < hiddenLink.length; ++i){
@@ -13,6 +13,15 @@ downArrowDiv.addEventListener('click', function(){
 })
 
 settingSvg.addEventListener('click', function(){
-    settingsDiv.style.display = "block"
-
+    if(settingsDiv.style.display == "block")
+    settingsDiv.style.display = "none"
+    else settingsDiv.style.display = "block"
+    
 })
+themeChanger.onchange = function(){
+    if(themeChanger.value == "thm_dark")
+    document.body.setAttribute('id', 'dark')
+    else if(themeChanger.value == "thm_beauty")
+    document.body.setAttribute('id', 'beauty')
+
+}
